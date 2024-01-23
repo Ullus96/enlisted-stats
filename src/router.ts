512e6d04680b0d377f87f5f48ebe07a938795395
Ullus <nikita96piter@gmail.com>
2024-01-23 03:03:55 +0300

    added router guard and titles for each page

--- src/router.ts
+++ src/router.ts
@@ -1,36 +1,96 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import SoldiersView from '@/views/SoldiersView.vue';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
-export default createRouter({
+const router = createRouter({
 	history: createWebHistory(),
 	routes: [
 		{
 			path: '/',
 			component: SoldiersView,
+			meta: {
+				title: 'Таблица бойцов',
+			},
 		},
 		{
 			path: '/events',
 			component: () => import('@/views/EventsView.vue'),
+			meta: {
+				title: 'События',
+			},
 		},
 		{
 			path: '/calculator',
 			component: () => import('@/views/CustomCalculatorView.vue'),
+			meta: {
+				title: 'Калькулятор',
+			},
 		},
 		{
 			path: '/skill-builds',
 			component: () => import('@/views/SkillBuildsView.vue'),
+			meta: {
+				title: 'Список сборок',
+			},
 		},
 		{
 			path: '/my-builds',
 			component: () => import('@/views/MyBuildsView.vue'),
+			meta: {
+				title: 'Мои сборки',
+				requiresAuth: true,
+			},
 		},
 		{
 			path: '/saved-builds',
 			component: () => import('@/views/SavedBuildsView.vue'),
+			meta: {
+				title: 'Сохраненные сборки',
+				requiresAuth: true,
+			},
 		},
 		{
 			path: '/profile',
 			component: () => import('@/views/ProfileView.vue'),
+			meta: {
+				title: 'Профиль',
+				requiresAuth: true,
+			},
 		},
 	],
 });
+
+const getCurrentUser = () => {
+	return new Promise((resolve, reject) => {
+		const removeListener = onAuthStateChanged(
+			getAuth(),
+			(user) => {
+				removeListener();
+				resolve(user);
+			},
+			reject
+		);
+	});
+};
+
+router.beforeEach(async (to, from, next) => {
+	// Get the page title from the route meta data that we have defined
+	const title: any = to.meta.title;
+	// If the route has a title, set it as the page title of the document/page
+	if (title) {
+		document.title = title;
+	}
+
+	if (to.matched.some((record) => record.meta.requiresAuth)) {
+		if (await getCurrentUser()) {
+			next();
+		} else {
+			console.log('You are not signed in');
+			next('/');
+		}
+	} else {
+		next();
+	}
+});
+
+export default router;
