8067d75e95d65c4262adb748a7cd236a2bb7870c
Ullus <nikita96piter@gmail.com>
2024-06-20 02:24:43 +0300

    Add router links structure with icons

--- src/components/TheAside.vue
+++ src/components/TheAside.vue
@@ -8,31 +8,89 @@
 		<div class="aside__line aside__line--top"></div>
 		<div class="aside__main-content">
 			<nav class="aside__navigation">
-				<ul class="aside__links">
-					<li class="aside__link">
-						<icon-base :iconName="cog">
-							<icon-cog></icon-cog>
-						</icon-base>
-					</li>
-					<li class="aside__link">2</li>
-					<li class="aside__link">3</li>
-					<li class="aside__link">3</li>
-				</ul>
+				<section class="aside__links">
+					<router-link class="aside__link" to="/">
+						<IconBase :iconName="'Table'">
+							<IconAngleDoubleUp />
+						</IconBase>
+						<span class="aside__link-desc">Таблица</span>
+					</router-link>
+
+					<router-link class="aside__link" to="/">
+						<IconBase :iconName="'Calculator'">
+							<IconUserCog />
+						</IconBase>
+						<span class="aside__link-desc">Калькулятор</span>
+					</router-link>
+
+					<router-link class="aside__link" to="/">
+						<IconBase :iconName="'Skill Builds'">
+							<IconBookOpen />
+						</IconBase>
+						<span class="aside__link-desc">Сборки</span>
+					</router-link>
+
+					<router-link class="aside__link" to="/">
+						<IconBase :iconName="'Events Calendar'">
+							<IconCalendar />
+						</IconBase>
+						<span class="aside__link-desc">Календарь событий</span>
+					</router-link>
+				</section>
+
 				<div class="aside__line"></div>
-				<ul class="aside__links">
-					<li class="aside__link">1</li>
-					<li class="aside__link">1</li>
-				</ul>
+
+				<section class="aside__links">
+					<router-link class="aside__link" to="/">
+						<IconBase :iconName="'My Builds'">
+							<IconHammer />
+						</IconBase>
+						<span class="aside__link-desc">Мои сборки</span>
+					</router-link>
+
+					<router-link class="aside__link" to="/">
+						<IconBase :iconName="'Saved Builds'">
+							<IconBookmark />
+						</IconBase>
+						<span class="aside__link-desc">Сохраненные</span>
+					</router-link>
+				</section>
+
 				<div class="aside__line"></div>
-				<ul class="aside__links">
-					<li class="aside__link">1</li>
-				</ul>
+
+				<section class="aside__links">
+					<router-link class="aside__link" to="/">
+						<IconBase :iconName="'Admin Panel'">
+							<IconCrown />
+						</IconBase>
+						<span class="aside__link-desc">Админка</span>
+					</router-link>
+				</section>
+
 				<div class="aside__line"></div>
-				<ul class="aside__links">
-					<li class="aside__link">1</li>
-					<li class="aside__link">1</li>
-					<li class="aside__link">1</li>
-				</ul>
+
+				<section class="aside__links">
+					<router-link class="aside__link" to="/">
+						<IconBase :iconName="'Settings'">
+							<IconCog />
+						</IconBase>
+						<span class="aside__link-desc">Настройки</span>
+					</router-link>
+
+					<button class="aside__link" to="/">
+						<IconBase :iconName="'Discord'">
+							<IconDiscord />
+						</IconBase>
+						<span class="aside__link-desc">Контакты</span>
+					</button>
+
+					<button class="aside__link" to="/">
+						<IconBase :iconName="'Skill Builds'">
+							<IconSignOut />
+						</IconBase>
+						<span class="aside__link-desc">Выйти</span>
+					</button>
+				</section>
 			</nav>
 
 			<footer class="aside__footer">
@@ -49,10 +107,31 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import IconBase from '@/components/ui/icons/IconBase.vue';
+import IconAngleDoubleUp from '@/components/ui/icons/IconAngleDoubleUp.vue';
+import IconUserCog from '@/components/ui/icons/IconUserCog.vue';
+import IconBookOpen from '@/components/ui/icons/IconBookOpen.vue';
+import IconCalendar from '@/components/ui/icons/IconCalendar.vue';
+import IconHammer from '@/components/ui/icons/IconHammer.vue';
+import IconBookmark from '@/components/ui/icons/IconBookmark.vue';
+import IconCrown from '@/components/ui/icons/IconCrown.vue';
 import IconCog from '@/components/ui/icons/IconCog.vue';
+import IconDiscord from '@/components/ui/icons/IconDiscord.vue';
+import IconSignOut from '@/components/ui/icons/IconSignOut.vue';
 
 export default defineComponent({
-	components: { IconBase, IconCog },
+	components: {
+		IconBase,
+		IconAngleDoubleUp,
+		IconUserCog,
+		IconBookOpen,
+		IconCalendar,
+		IconHammer,
+		IconBookmark,
+		IconCrown,
+		IconCog,
+		IconDiscord,
+		IconSignOut,
+	},
 	setup() {},
 });
 </script>
