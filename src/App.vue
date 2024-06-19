f04e38fc46bc4b242873c75912066942d1f21ba7
Ullus <nikita96piter@gmail.com>
2024-06-20 00:42:08 +0300

    Add basic structure for navigation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
 	<div class="page-wrapper">
-		<the-header></the-header>
+		<!-- <the-header></the-header> -->
+		<the-aside></the-aside>
 		<main class="main">
 			<router-view></router-view>
 		</main>
-		<the-footer></the-footer>
+		<!-- <the-footer></the-footer> -->
 	</div>
 </template>
 
 <script lang="ts">
+import TheAside from './components/TheAside.vue';
 import TheHeader from './components/TheHeader.vue';
 import TheFooter from './components/TheFooter.vue';
 import { defineComponent } from 'vue';
@@ -16,6 +18,7 @@ import { defineComponent } from 'vue';
 export default defineComponent({
 	name: 'App',
 	components: {
+		TheAside,
 		TheHeader,
 		TheFooter,
 	},
---
+++ src/components/TheAside.vue
@@ -0,0 +1,51 @@
+<template>
+	<aside class="aside">
+		<img
+			src="https://i.imgur.com/T39kQkM.jpeg"
+			alt="Profile Picture"
+			class="aside__avatar"
+		/>
+		<div class="aside__line aside__line--top"></div>
+		<div class="aside__main-content">
+			<nav class="aside__navigation">
+				<ul class="aside__links">
+					<li class="aside__link">1</li>
+					<li class="aside__link">2</li>
+					<li class="aside__link">3</li>
+					<li class="aside__link">3</li>
+				</ul>
+				<div class="aside__line"></div>
+				<ul class="aside__links">
+					<li class="aside__link">1</li>
+					<li class="aside__link">1</li>
+				</ul>
+				<div class="aside__line"></div>
+				<ul class="aside__links">
+					<li class="aside__link">1</li>
+				</ul>
+				<div class="aside__line"></div>
+				<ul class="aside__links">
+					<li class="aside__link">1</li>
+					<li class="aside__link">1</li>
+					<li class="aside__link">1</li>
+				</ul>
+			</nav>
+
+			<footer class="aside__footer">
+				<div class="aside__footer-legal">
+					<p>1</p>
+					<p>2</p>
+				</div>
+				<div class="aside__footer-links">1 | 2</div>
+			</footer>
+		</div>
+	</aside>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	setup() {},
+});
+</script>
