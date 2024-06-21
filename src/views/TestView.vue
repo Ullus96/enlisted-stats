164bae0eb296e93da9a03317843033940e49d948
Ullus <nikita96piter@gmail.com>
2024-06-21 20:43:43 +0300

    Add button styles

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,5 +1,41 @@
 <template>
 	<div class="container mt-l">
-		<button class="btn">Test</button>
+		<div
+			class="flex"
+			style="
+				display: flex;
+				flex-direction: column;
+				gap: 20px;
+				width: fit-content;
+				margin: 0 auto;
+			"
+		>
+			<div>
+				<button class="btn">Test</button>
+			</div>
+			<div>
+				<button class="btn" disabled>Disabled</button>
+			</div>
+			Normal:
+			<div>
+				<button class="btn btn-m btn-primary">Medium</button>
+			</div>
+			<div>
+				<button class="btn btn-m btn-secondary">Medium</button>
+			</div>
+			<div>
+				<button class="btn btn-m btn-tertiary">Medium</button>
+			</div>
+			Small:
+			<div>
+				<button class="btn btn-sm btn-primary">Small</button>
+			</div>
+			<div>
+				<button class="btn btn-sm btn-secondary">Small</button>
+			</div>
+			<div>
+				<button class="btn btn-sm btn-tertiary">Small</button>
+			</div>
+		</div>
 	</div>
 </template>
