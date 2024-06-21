f7625ad623b23346a8ba2e40f22f95419b076b25
Ullus <nikita96piter@gmail.com>
2024-06-22 00:44:15 +0300

    Add hide button to vuex debug

--- src/components/debug/VuexDebug.vue
+++ src/components/debug/VuexDebug.vue
@@ -1,5 +1,14 @@
 <template>
-	<div class="debug">
+	<div class="debug" v-if="isVisible">
+		<button class="btn btn-sm btn-tertiary" @click="isVisible = !isVisible">
+			Hide
+		</button>
+		<p class="debug__title">LoadingData:</p>
+		<p v-for="(line, key) in store.state.loading" :key="line">
+			{{ key }}: {{ line }}
+		</p>
+		<div class="aside__line debug__line"></div>
+		<p class="debug__title">UserData:</p>
 		<p v-for="(line, key) in store.state.user" :key="line">
 			{{ key }}: {{ line }}
 		</p>
@@ -7,14 +16,15 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, Ref, ref } from 'vue';
 import { useStore } from 'vuex';
 
 export default defineComponent({
 	setup() {
+		const isVisible: Ref<boolean> = ref(true);
 		const store = useStore();
 
-		return { store };
+		return { isVisible, store };
 	},
 });
 </script>
