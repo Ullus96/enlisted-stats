181df5d8f0ec25faacd327a689f19afee94a5d6e
Ullus <nikita96piter@gmail.com>
2024-06-25 06:31:48 +0300

    Add styles to registration modal

--- src/components/login/LoginOrRegister.vue
+++ src/components/login/LoginOrRegister.vue
@@ -2,10 +2,14 @@
 	<Teleport to="#visible-screen-40">
 		<div class="page-mask page-mask--above" @click.stop="forceClose"></div>
 		<section class="modal">
-			<button class="btn btn-tertiary btn-sm">x</button>
-			<h3 class="modal__title">Войти</h3>
+			<button class="btn btn-tertiary btn-sm modal__btn">
+				<IconBase :iconName="'Close modal window'">
+					<IconTimes />
+				</IconBase>
+			</button>
+			<h3 class="modal__title">Войти или зарегистрироваться</h3>
 			<!-- google btn -->
-			<div class="">Кнопка</div>
+			<div class="modal__google-btn">Кнопка</div>
 			<p class="modal__legal">
 				Регистрируясь на сайте, Вы соглашаетесь с
 				<router-link to="/user-agreement" class="modal__link"
@@ -23,8 +27,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useStore } from 'vuex';
+import IconBase from '@/components/ui/icons/IconBase.vue';
+import IconTimes from '@/components/ui/icons/IconTimes.vue';
 
 export default defineComponent({
+	components: { IconBase, IconTimes },
 	setup() {
 		const store = useStore();
 
