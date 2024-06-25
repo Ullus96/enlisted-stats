cad54b351dab98aba5d7fb75bd878973c816af8a
Ullus <nikita96piter@gmail.com>
2024-06-25 07:26:10 +0300

    Extract modal base structure to separate component

--- src/components/login/LoginOrRegister.vue
+++ src/components/login/LoginOrRegister.vue
@@ -1,50 +1,48 @@
 <template>
-	<Teleport to="#visible-screen-40">
-		<div class="page-mask page-mask--above" @click.stop="forceClose"></div>
-		<section class="modal">
-			<button class="btn btn-tertiary btn-sm modal__btn" @click="forceClose">
-				<IconBase :iconName="'Close modal window'">
-					<IconTimes />
-				</IconBase>
-			</button>
-			<h3 class="modal__title">Войти или зарегистрироваться</h3>
+	<ModalComponent @closeModal="forceClose">
+		<h3 class="modal__title">Войти или зарегистрироваться</h3>
 
-			<button class="modal__google-btn btn">
-				<IconBase
-					:iconName="'Sign in with Google'"
-					class="modal__google-icon"
-					:width="32"
-					:height="32"
-					:viewBox="`0 0 32 32`"
-				>
-					<IconGoogle />
-				</IconBase>
-				<span class="modal__google-text">Войти при помощи Google</span>
-			</button>
+		<button class="modal__google-btn btn">
+			<IconBase
+				:iconName="'Sign in with Google'"
+				class="modal__google-icon"
+				:width="32"
+				:height="32"
+				:viewBox="`0 0 32 32`"
+			>
+				<IconGoogle />
+			</IconBase>
+			<span class="modal__google-text">Войти при помощи Google</span>
+		</button>
 
-			<p class="modal__legal">
-				Регистрируясь на сайте, Вы соглашаетесь с
-				<router-link to="/user-agreement" class="modal__link"
-					>пользовательским соглашением</router-link
-				>
-				и
-				<router-link to="/privacy" class="modal__link"
-					>политикой обработки персональных данных</router-link
-				>.
-			</p>
-		</section>
-	</Teleport>
+		<p class="modal__legal">
+			Регистрируясь на сайте, Вы соглашаетесь с
+			<router-link
+				@click="closeModalAndNavigation"
+				to="/user-agreement"
+				class="modal__link"
+				>пользовательским соглашением</router-link
+			>
+			и
+			<router-link
+				@click="closeModalAndNavigation"
+				to="/privacy"
+				class="modal__link"
+				>политикой обработки персональных данных</router-link
+			>.
+		</p>
+	</ModalComponent>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { useStore } from 'vuex';
+import ModalComponent from '@/components/ui/ModalComponent.vue';
 import IconBase from '@/components/ui/icons/IconBase.vue';
-import IconTimes from '@/components/ui/icons/IconTimes.vue';
 import IconGoogle from '@/components/ui/icons/IconGoogle.vue';
 
 export default defineComponent({
-	components: { IconBase, IconTimes, IconGoogle },
+	components: { ModalComponent, IconBase, IconGoogle },
 	setup() {
 		const store = useStore();
 
@@ -52,8 +50,14 @@ export default defineComponent({
 			store.commit('toggleLoginModalVisibility', true);
 		}
 
+		function closeModalAndNavigation() {
+			store.commit('toggleLoginModalVisibility', true);
+			store.commit('toggleNavigationVisibility', true);
+		}
+
 		return {
 			forceClose,
+			closeModalAndNavigation,
 		};
 	},
 });
---
+++ src/components/ui/ModalComponent.vue
@@ -0,0 +1,32 @@
+<template>
+	<Teleport to="#visible-screen-40">
+		<div class="page-mask page-mask--above" @click.stop="closeModal"></div>
+		<section class="modal">
+			<button class="btn btn-tertiary btn-sm modal__btn" @click="closeModal">
+				<IconBase :iconName="'Close modal window'">
+					<IconTimes />
+				</IconBase>
+			</button>
+
+			<slot></slot>
+		</section>
+	</Teleport>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import IconBase from '@/components/ui/icons/IconBase.vue';
+import IconTimes from '@/components/ui/icons/IconTimes.vue';
+
+export default defineComponent({
+	components: { IconBase, IconTimes },
+	emits: ['closeModal'],
+	setup(props, context) {
+		function closeModal() {
+			context.emit('closeModal');
+		}
+
+		return { closeModal };
+	},
+});
+</script>
