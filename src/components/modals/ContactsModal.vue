f023b7a15f736ddaf6544c27ebf2bfa4a129c3c1
Ullus <nikita96piter@gmail.com>
2024-06-25 09:34:12 +0300

    Fill Contacts modal with content

--- src/components/modals/ContactsModal.vue
+++ src/components/modals/ContactsModal.vue
@@ -1,16 +1,50 @@
 <template>
 	<ModalComponent :modalName="'isContactsModalVisible'">
 		<h3 class="modal__title">Связаться с автором сайта</h3>
+
+		<div class="modal__body">
+			<p class="modal__desc">
+				Если есть какие-то вопросы, или если тобой были замечены какие-то
+				несостыковки в цифрах:
+			</p>
+			<div class="modal__contacts">
+				<p class="modal__contact">
+					<IconBase :iconName="'Discord'" :width="24" :height="24">
+						<IconDiscord />
+					</IconBase>
+					<span>@ullus77</span>
+				</p>
+
+				<p class="modal__contact modal__link">
+					<IconBase :iconName="'Enlisted Forum'" :width="24" :height="24">
+						<IconLetter />
+					</IconBase>
+					<a href="https://forum.enlisted.net/ru/u/Ullus/"
+						>forum.enlisted.net/ru/u/Ullus/</a
+					>
+				</p>
+			</div>
+
+			<p class="modal__desc">
+				Дискорд предпочтительнее, т.к. форум посещаю редко.
+			</p>
+		</div>
 	</ModalComponent>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import ModalComponent from '@/components/ui/ModalComponent.vue';
+import IconBase from '@/components/ui/icons/IconBase.vue';
+import IconDiscord from '@/components/ui/icons/IconDiscord.vue';
+import IconLetter from '@/components/ui/icons/IconLetter.vue';
 
 export default defineComponent({
 	components: {
 		ModalComponent,
+		IconBase,
+		IconDiscord,
+		IconLetter,
 	},
 	setup() {
 		return {};
