43d8bc0664cc5c5b56d4940316766217f0e26bba
Ullus <nikita96piter@gmail.com>
2024-06-27 07:27:08 +0300

    Add basic admin page structure & Add tab switching functionality with Transition animations

--- src/views/AdminPanelView.vue
+++ src/views/AdminPanelView.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="container-sm mt-s">
+	<!-- <div class="container-sm mt-s">
 		<div class="profile__header-admin">
 			<h1 class="profile__header-title">Панель администратора</h1>
 			<div class="profile__tabs">
@@ -24,6 +24,50 @@
 			<p v-if="!activeTab">Выбери какой-нибудь пункт</p>
 			<admin-events v-if="activeTab == 'events'"></admin-events>
 		</div>
+	</div> -->
+	<div class="container mt-l">
+		<div class="container-sm">
+			<h3>Панель администратора</h3>
+
+			<div class="admin__tabs">
+				<button
+					@click="setActiveTab('events')"
+					class="btn btn-tertiary admin__tab"
+					:class="{ active: activeTab == 'events' }"
+				>
+					События
+				</button>
+				<button
+					@click="setActiveTab('placeholder')"
+					class="btn btn-tertiary admin__tab"
+					:class="{ active: activeTab == 'placeholder' }"
+				>
+					Placeholder
+				</button>
+			</div>
+
+			<transition name="slide-fade" mode="out-in">
+				<section v-if="!activeTab" key="no-category" class="admin__category">
+					<p class="admin__category-title">Выбери какую-либо категорию</p>
+				</section>
+
+				<section
+					v-else-if="activeTab === 'events'"
+					key="events"
+					class="admin__category"
+				>
+					<p class="admin__category-title">События</p>
+				</section>
+
+				<section
+					v-else-if="activeTab === 'placeholder'"
+					key="placeholder"
+					class="admin__category"
+				>
+					<p class="admin__category-title">Что-то еще</p>
+				</section>
+			</transition>
+		</div>
 	</div>
 </template>
 
@@ -35,8 +79,9 @@ import LowerPopUp from '@/components/error/LowerPopUp.vue';
 export default defineComponent({
 	components: { LowerPopUp, AdminEvents },
 	setup() {
-		type TActiveTab = 'events' | 'users';
+		type TActiveTab = 'events' | 'placeholder';
 		const activeTab: Ref<TActiveTab | null> = ref(null);
+		const animationStart: Ref<boolean> = ref(false);
 
 		function setActiveTab(name: TActiveTab) {
 			activeTab.value = name;
@@ -45,7 +90,26 @@ export default defineComponent({
 		return {
 			activeTab,
 			setActiveTab,
+			animationStart,
 		};
 	},
 });
 </script>
+
+<style>
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+	transition: opacity 0.4s ease-in-out, transform 0.3s ease-in-out;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+	transform: translateX(-10rem);
+	opacity: 0;
+}
+.slide-fade-enter-to,
+.slide-fade-leave-from {
+	transform: translateX(0rem);
+	opacity: 1;
+}
+</style>
