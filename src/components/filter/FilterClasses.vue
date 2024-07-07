5f94a8a62c2bf81365c69a2b030bcfa83e0b533e
Ullus <nikita96piter@gmail.com>
2024-07-08 00:15:30 +0300

    Change layout of class filter to column

--- src/components/filter/FilterClasses.vue
+++ src/components/filter/FilterClasses.vue
@@ -1,216 +1,350 @@
 <template>
 	<div class="filter__classes-block">
-		<div class="filter__group">
+		<div class="filter__group filter__group--btns-only">
 			<button
-				class="filter__class filter__class--no-icons btn"
+				class="filter__class filter__class--no-icons btn btn-m btn-tertiary"
 				@click="removeClass"
 			>
 				Все
 			</button>
 			<button
-				class="filter__class filter__class--no-icons btn"
+				class="filter__class filter__class--no-icons btn btn-m btn-tertiary"
 				@click="setClass('custom')"
 			>
 				Акционные
 			</button>
 		</div>
-		<div class="filter__group">
-			<button class="filter__class gap-none btn" @click="setClass('rifleman1')">
-				<img src="@/assets/soldier_icons/rifleman.svg" class="filter__icon" />
-				<span>Боец I</span>
-			</button>
-			<button class="filter__class gap-none btn" @click="setClass('rifleman2')">
-				<img src="@/assets/soldier_icons/rifleman.svg" class="filter__icon" />
-				<span>Боец II</span>
-			</button>
-			<button class="filter__class gap-none btn" @click="setClass('rifleman3')">
-				<img src="@/assets/soldier_icons/rifleman.svg" class="filter__icon" />
-				<span>Боец III</span>
-			</button>
-		</div>
 
 		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('medic')">
+			<div class="filter__group-title">
+				<img src="@/assets/soldier_icons/rifleman.svg" class="filter__icon" />
+				<span>Боец: </span>
+			</div>
+			<div class="filter__group-levels">
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('rifleman1')"
+				>
+					<span>I</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('rifleman2')"
+				>
+					<span>II</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('rifleman3')"
+				>
+					<span>III</span>
+				</button>
+			</div>
+		</div>
+
+		<div class="filter__group filter__group--btns-only">
+			<button
+				class="filter__class btn btn-m btn-tertiary"
+				@click="setClass('medic')"
+			>
 				<img src="@/assets/soldier_icons/medic.svg" class="filter__icon" />
 				<span>Медик</span>
 			</button>
 		</div>
 
 		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('assaulter1')">
+			<div class="filter__group-title">
 				<img src="@/assets/soldier_icons/assaulter.svg" class="filter__icon" />
-				<span>Штурмовик I</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('assaulter2')">
-				<img src="@/assets/soldier_icons/assaulter.svg" class="filter__icon" />
-				<span>Штурмовик II</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('assaulter3')">
-				<img src="@/assets/soldier_icons/assaulter.svg" class="filter__icon" />
-				<span>Штурмовик III</span>
-			</button>
-			<div class="filter__class btn" @click="setClass('assaulter4')">
-				<img src="@/assets/soldier_icons/assaulter.svg" class="filter__icon" />
-				<span>Штурмовик IV</span>
+				<span>Штурмовик: </span>
+			</div>
+
+			<div class="filter__group-levels">
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('assaulter1')"
+				>
+					<span>I</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('assaulter2')"
+				>
+					<span>II</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('assaulter3')"
+				>
+					<span>III</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('assaulter4')"
+				>
+					<span>IV</span>
+				</button>
 			</div>
 		</div>
 
 		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('engineer1')">
+			<div class="filter__group-title">
 				<img src="@/assets/soldier_icons/engineer.svg" class="filter__icon" />
-				<span>Инженер I</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('engineer2')">
-				<img src="@/assets/soldier_icons/engineer.svg" class="filter__icon" />
-				<span>Инженер II</span>
-			</button>
+				<span>Инженер: </span>
+			</div>
+
+			<div class="filter__group-levels">
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('engineer1')"
+				>
+					<span>I</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('engineer2')"
+				>
+					<span>II</span>
+				</button>
+			</div>
 		</div>
 
 		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('sniper1')">
+			<div class="filter__group-title">
 				<img src="@/assets/soldier_icons/sniper.svg" class="filter__icon" />
-				<span>Снайпер I</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('sniper2')">
-				<img src="@/assets/soldier_icons/sniper.svg" class="filter__icon" />
-				<span>Снайпер II</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('sniper3')">
-				<img src="@/assets/soldier_icons/sniper.svg" class="filter__icon" />
-				<span>Снайпер III</span>
-			</button>
+				<span>Снайпер: </span>
+			</div>
+
+			<div class="filter__group-levels">
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('sniper1')"
+				>
+					<span>I</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('sniper2')"
+				>
+					<span>II</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('sniper3')"
+				>
+					<span>III</span>
+				</button>
+			</div>
 		</div>
 
 		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('at1')">
+			<div class="filter__group-title">
 				<img src="@/assets/soldier_icons/antitank.svg" class="filter__icon" />
-				<span>Бронебойщик I</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('at2')">
-				<img src="@/assets/soldier_icons/antitank.svg" class="filter__icon" />
-				<span>Бронебойщик II</span>
-			</button>
+				<span>Бронебойщик: </span>
+			</div>
+
+			<div class="filter__group-levels">
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('at1')"
+				>
+					<span>I</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('at2')"
+				>
+					<span>II</span>
+				</button>
+			</div>
 		</div>
 
 		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('mg1')">
+			<div class="filter__group-title">
 				<img src="@/assets/soldier_icons/mg.svg" class="filter__icon" />
-				<span>Пулемётчик I</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('mg2')">
-				<img src="@/assets/soldier_icons/mg.svg" class="filter__icon" />
-				<span>Пулемётчик II</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('mg3')">
-				<img src="@/assets/soldier_icons/mg.svg" class="filter__icon" />
-				<span>Пулемётчик III</span>
-			</button>
+				<span>Пулемётчик: </span>
+			</div>
+
+			<div class="filter__group-levels">
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('mg1')"
+				>
+					<span>I</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('mg2')"
+				>
+					<span>II</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('mg3')"
+				>
+					<span>III</span>
+				</button>
+			</div>
 		</div>
 
 		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('radio1')">
+			<div class="filter__group-title">
 				<img src="@/assets/soldier_icons/radio.svg" class="filter__icon" />
-				<span>Радист I</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('radio2')">
-				<img src="@/assets/soldier_icons/radio.svg" class="filter__icon" />
-				<span>Радист II</span>
-			</button>
+				<span>Радист: </span>
+			</div>
+
+			<div class="filter__group-levels">
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('radio1')"
+				>
+					<span>I</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('radio2')"
+				>
+					<span>II</span>
+				</button>
+			</div>
 		</div>
 
-		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('mortar')">
+		<div class="filter__group filter__group--btns-only">
+			<button
+				class="filter__class btn btn-m btn-tertiary"
+				@click="setClass('mortar')"
+			>
 				<img src="@/assets/soldier_icons/mortar.svg" class="filter__icon" />
 				<span>Миномётчик</span>
 			</button>
 		</div>
 
 		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('flamethrower1')">
+			<div class="filter__group-title">
 				<img
 					src="@/assets/soldier_icons/flamethrower.svg"
 					class="filter__icon"
 				/>
-				<span>Огнемётчик I</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('flamethrower2')">
-				<img
-					src="@/assets/soldier_icons/flamethrower.svg"
-					class="filter__icon"
-				/>
-				<span>Огнемётчик II</span>
-			</button>
+				<span>Огнемётчик: </span>
+			</div>
+
+			<div class="filter__group-levels">
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('flamethrower1')"
+				>
+					<span>I</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('flamethrower2')"
+				>
+					<span>II</span>
+				</button>
+			</div>
 		</div>
 
 		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('pilot-fighter1')">
+			<div class="filter__group-title">
 				<img
 					src="@/assets/soldier_icons/pilot-fighter.svg"
 					class="filter__icon"
 				/>
-				<span>Пилот-истр. I</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('pilot-fighter2')">
-				<img
-					src="@/assets/soldier_icons/pilot-fighter.svg"
-					class="filter__icon"
-				/>
-				<span>Пилот-истр. II</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('pilot-fighter3')">
-				<img
-					src="@/assets/soldier_icons/pilot-fighter.svg"
-					class="filter__icon"
-				/>
-				<span>Пилот-истр. III</span>
-			</button>
+				<span>Пилот-истребитель: </span>
+			</div>
+
+			<div class="filter__group-levels">
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('pilot-fighter1')"
+				>
+					<span>I</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('pilot-fighter2')"
+				>
+					<span>II</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('pilot-fighter3')"
+				>
+					<span>III</span>
+				</button>
+			</div>
 		</div>
 
 		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('pilot-attacker1')">
+			<div class="filter__group-title">
 				<img
 					src="@/assets/soldier_icons/pilot-attacker.svg"
 					class="filter__icon"
 				/>
-				<span>Пилот-штурм. I</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('pilot-attacker2')">
-				<img
-					src="@/assets/soldier_icons/pilot-attacker.svg"
-					class="filter__icon"
-				/>
-				<span>Пилот-штурм. II</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('pilot-attacker3')">
-				<img
-					src="@/assets/soldier_icons/pilot-attacker.svg"
-					class="filter__icon"
-				/>
-				<span>Пилот-штурм. III</span>
-			</button>
+				<span>Пилот-штурмовик: </span>
+			</div>
+
+			<div class="filter__group-levels">
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('pilot-attacker1')"
+				>
+					<span> I</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('pilot-attacker2')"
+				>
+					<span>II</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('pilot-attacker3')"
+				>
+					<span>III</span>
+				</button>
+			</div>
 		</div>
 
 		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('tank1')">
+			<div class="filter__group-title">
 				<img src="@/assets/soldier_icons/tank.svg" class="filter__icon" />
-				<span>Танкист I</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('tank2')">
-				<img src="@/assets/soldier_icons/tank.svg" class="filter__icon" />
-				<span>Танкист II</span>
-			</button>
-			<button class="filter__class btn" @click="setClass('tank3')">
-				<img src="@/assets/soldier_icons/tank.svg" class="filter__icon" />
-				<span>Танкист III</span>
-			</button>
+				<span>Танкист: </span>
+			</div>
+
+			<div class="filter__group-levels">
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('tank1')"
+				>
+					<span>I</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('tank2')"
+				>
+					<span>II</span>
+				</button>
+				<button
+					class="filter__class btn btn-m btn-tertiary"
+					@click="setClass('tank3')"
+				>
+					<span>III</span>
+				</button>
+			</div>
 		</div>
 
-		<div class="filter__group">
-			<button class="filter__class btn" @click="setClass('moto1')">
+		<div class="filter__group filter__group--btns-only">
+			<button
+				class="filter__class btn btn-m btn-tertiary"
+				@click="setClass('moto1')"
+			>
 				<img src="@/assets/soldier_icons/moto.svg" class="filter__icon" />
 				<span>Мотоциклист</span>
 			</button>
-			<button class="filter__class btn" @click="setClass('apc-driver')">
+			<button
+				class="filter__class btn btn-m btn-tertiary"
+				@click="setClass('apc-driver')"
+			>
 				<img src="@/assets/soldier_icons/apc-driver.svg" class="filter__icon" />
 				<span>Водитель БТР</span>
 			</button>
