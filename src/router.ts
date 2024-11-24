a7c22d04b10c1613729ba453a7fa6576f078d3a7
Ullus <nikita96piter@gmail.com>
2024-11-24 03:00:02 +0300

    Add types to routes in router

--- src/router.ts
+++ src/router.ts
@@ -1,113 +1,121 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import SoldiersView from '@/views/SoldiersView.vue';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { checkIfUserAnAdmin } from '@/components/auth/functions/checkIfUserAnAdmin';
 import { adminsList } from './firebase/adminsList';
 
+export interface ICustomRouteMeta {
+	title: string;
+	requiresAuth?: boolean;
+	requiredAdminRights?: boolean;
+}
+
+const routes: Array<RouteRecordRaw & { meta?: ICustomRouteMeta }> = [
+	{
+		path: '/',
+		name: 'Home',
+		component: SoldiersView,
+		meta: {
+			title: 'Таблица бойцов',
+		},
+	},
+	{
+		path: '/events',
+		name: 'Events',
+		component: () => import('@/views/EventsView.vue'),
+		meta: {
+			title: 'События',
+		},
+	},
+	{
+		path: '/calculator',
+		name: 'Calculator',
+		component: () => import('@/views/CustomCalculatorView.vue'),
+		meta: {
+			title: 'Калькулятор',
+		},
+	},
+	{
+		path: '/skill-builds',
+		name: 'Skill builds',
+		component: () => import('@/views/SkillBuildsView.vue'),
+		meta: {
+			title: 'Список сборок',
+		},
+	},
+	{
+		path: '/build/:id',
+		name: 'Build',
+		component: () => import('@/views/SingleBuildView.vue'),
+		meta: {
+			title: 'Сборка',
+		},
+	},
+	{
+		path: '/test',
+		name: 'Test page',
+		component: () => import('@/views/TestView.vue'),
+		meta: {
+			title: 'Тестовая страница',
+		},
+	},
+	{
+		path: '/privacy',
+		name: 'Privacy policy',
+		component: () => import('@/views/legal/PrivacyView.vue'),
+		meta: {
+			title: 'Политика конфиденциальности',
+		},
+	},
+	{
+		path: '/user-agreement',
+		name: 'User agreement',
+		component: () => import('@/views/legal/UserAgreementView.vue'),
+		meta: {
+			title: 'Пользовательское соглашение',
+		},
+	},
+	{
+		path: '/my-builds',
+		name: 'My builds',
+		component: () => import('@/views/MyBuildsView.vue'),
+		meta: {
+			title: 'Мои сборки',
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/saved-builds',
+		name: 'Saved builds',
+		component: () => import('@/views/SavedBuildsView.vue'),
+		meta: {
+			title: 'Сохраненные сборки',
+			requiresAuth: true,
+		},
+	},
+	{
+		path: '/settings',
+		name: 'Settings',
+		component: () => import('@/views/ProfileView.vue'),
+		meta: {
+			title: 'Настройки',
+		},
+	},
+	{
+		path: '/admin',
+		name: 'Admin',
+		component: () => import('@/views/AdminPanelView.vue'),
+		meta: {
+			title: 'Панель администратора',
+			requiresAuth: true,
+			requiredAdminRights: true,
+		},
+	},
+];
+
 const router = createRouter({
 	history: createWebHistory(),
-	routes: [
-		{
-			path: '/',
-			name: 'Home',
-			component: SoldiersView,
-			meta: {
-				title: 'Таблица бойцов',
-			},
-		},
-		{
-			path: '/events',
-			name: 'Events',
-			component: () => import('@/views/EventsView.vue'),
-			meta: {
-				title: 'События',
-			},
-		},
-		{
-			path: '/calculator',
-			name: 'Calculator',
-			component: () => import('@/views/CustomCalculatorView.vue'),
-			meta: {
-				title: 'Калькулятор',
-			},
-		},
-		{
-			path: '/skill-builds',
-			name: 'Skill builds',
-			component: () => import('@/views/SkillBuildsView.vue'),
-			meta: {
-				title: 'Список сборок',
-			},
-		},
-		{
-			path: '/build/:id',
-			name: 'Build',
-			component: () => import('@/views/SingleBuildView.vue'),
-			meta: {
-				title: 'Сборка',
-			},
-		},
-		{
-			path: '/test',
-			name: 'Test page',
-			component: () => import('@/views/TestView.vue'),
-			meta: {
-				title: 'Тестовая страница',
-			},
-		},
-		{
-			path: '/privacy',
-			name: 'Privacy policy',
-			component: () => import('@/views/legal/PrivacyView.vue'),
-			meta: {
-				title: 'Политика конфиденциальности',
-			},
-		},
-		{
-			path: '/user-agreement',
-			name: 'User agreement',
-			component: () => import('@/views/legal/UserAgreementView.vue'),
-			meta: {
-				title: 'Пользовательское соглашение',
-			},
-		},
-		{
-			path: '/my-builds',
-			name: 'My builds',
-			component: () => import('@/views/MyBuildsView.vue'),
-			meta: {
-				title: 'Мои сборки',
-				requiresAuth: true,
-			},
-		},
-		{
-			path: '/saved-builds',
-			name: 'Saved builds',
-			component: () => import('@/views/SavedBuildsView.vue'),
-			meta: {
-				title: 'Сохраненные сборки',
-				requiresAuth: true,
-			},
-		},
-		{
-			path: '/settings',
-			name: 'Settings',
-			component: () => import('@/views/ProfileView.vue'),
-			meta: {
-				title: 'Настройки',
-			},
-		},
-		{
-			path: '/admin',
-			name: 'Admin',
-			component: () => import('@/views/AdminPanelView.vue'),
-			meta: {
-				title: 'Панель администратора',
-				requiresAuth: true,
-				requiredAdminRights: true,
-			},
-		},
-	],
+	routes,
 });
 
 const getCurrentUser = () => {
