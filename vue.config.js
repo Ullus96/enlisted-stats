143dbfe3e84c298624a3f65e908bcfa60c6c46a1
Ullus <nikita96piter@gmail.com>
2025-05-03 22:32:12 +0300

    Change webpack config to remove licenses and console logs from a bundle file

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
 	chainWebpack: (config) => {
+		// Существующая настройка для vue-loader
 		config.module
 			.rule('vue')
 			.use('vue-loader')
@@ -12,5 +13,21 @@ module.exports = defineConfig({
 				options.compilerOptions.isCustomElement = (tag) => tag === 'search';
 				return options;
 			});
+
+		// Новая настройка для Terser (удаление комментариев и минимизация)
+		config.optimization.minimizer('terser').tap((args) => {
+			args[0].terserOptions = {
+				...args[0].terserOptions,
+				compress: {
+					drop_console: true, // Не удаляем console.log (можно поставить true, если не нужны)
+					drop_debugger: true, // Удаляем debugger
+				},
+				mangle: true, // Сокращаем имена переменных
+				output: {
+					comments: false, // Удаляем все комментарии, включая лицензии
+				},
+			};
+			return args;
+		});
 	},
 });
