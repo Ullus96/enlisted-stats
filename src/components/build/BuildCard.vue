748f119cfb681a3f392212642927989535117957
Ullus <nikita96piter@gmail.com>
2025-05-14 00:31:53 +0300

    Base layout of a new build card design

--- src/components/build/BuildCard.vue
+++ src/components/build/BuildCard.vue
@@ -1,7 +1,7 @@
 <template>
 	<template v-if="isFinishedLoading && loadedUserData">
 		<section
-			class="sbuild__item sbuild__item--clickable"
+			class="build__item"
 			v-shiny-border="{
 				height: 2,
 				padding: 12,
@@ -9,20 +9,14 @@
 			}"
 			v-ripple
 		>
-			<router-link
-				:to="/build/ + item.dbId"
-				target="_blank"
-				class="sbuild__link"
-				v-if="hasLink"
-			>
-			</router-link>
-			<div class="sbuild__main-content">
-				<p class="sbuild__name">
+			<!-- Main -->
+			<div class="build__main">
+				<h4 class="build__title">
 					{{ item.data.name }}
-				</p>
+				</h4>
 
-				<div class="sbuild__data-block">
-					<div class="sbuild__data-class">
+				<div class="build__desc-wrapper">
+					<div class="build__class">
 						<!-- if soldier has a preset class -->
 						<template v-if="item.soldierClass !== 'custom'">
 							<img
@@ -31,7 +25,7 @@
 										getSoldierData('id', item.soldierClass, 'icon'))
 								"
 								alt=""
-								class="sbuild__data-icon"
+								class="build__class-icon"
 							/>
 							<p>
 								{{ getSoldierData('id', item.soldierClass, 'name') }}
@@ -43,7 +37,7 @@
 							<img
 								:src="require('@/assets/soldier_icons/tags.svg')"
 								alt=""
-								class="sbuild__data-icon"
+								class="build__class-icon"
 							/>
 							<p>
 								<span v-for="(tag, idx) in item.tags" :key="tag"
@@ -53,22 +47,22 @@
 							</p>
 						</template>
 					</div>
-					<div class="sbuild__data-stats">
-						<span class="sbuild__data-stat">
+					<div class="build__stats">
+						<span class="build__stat">
 							<IconBase :iconColor="'#94FF94'" :width="14" :height="14">
 								<IconMobility />
 							</IconBase>
 							{{ item.stats[0] }}
 						</span>
-						<span class="sbuild__data-separator">-</span>
-						<span class="sbuild__data-stat">
+						<span class="build__separator">-</span>
+						<span class="build__stat">
 							<IconBase :iconColor="'#FF9494'" :width="14" :height="14">
 								<IconMobility />
 							</IconBase>
 							{{ item.stats[1] }}
 						</span>
-						<span class="sbuild__data-separator">-</span>
-						<span class="sbuild__data-stat">
+						<span class="build__separator">-</span>
+						<span class="build__stat">
 							<IconBase :iconColor="'#FFFF94'" :width="14" :height="14">
 								<IconMobility />
 							</IconBase>
@@ -76,7 +70,22 @@
 						</span>
 					</div>
 				</div>
+			</div>
 
+			<!-- Buttons -->
+			<div class="build__buttons-wrapper" :class="{ fullheight: true }">
+				<div class="build__buttons-group">
+					<span>1</span>
+					<span>2</span>
+					<span>3</span>
+				</div>
+				<div class="build__buttons-group">
+					<span>1</span>
+				</div>
+			</div>
+
+			<!-- Avatar Panel -->
+			<div class="build__footer" :class="{ fullwidth: false }">
 				<template
 					v-if="
 						(loadedUserData &&
@@ -88,127 +97,53 @@
 						'none'
 					"
 				>
-					<div class="sbuild__author-block">
+					<div class="build__footer-wrapper">
 						<UserAvatar
 							:photo-u-r-l="String(loadedUserData.photoURL)"
 							:display-name="String(loadedUserData.displayName)"
 							:email-hash="String(loadedUserData.emailHash) || ''"
 							:avatar-provider="loadedUserData.avatarProvider || 'google'"
-							class="sbuild__author-avatar"
+							class="build__avatar"
 						/>
-						<div class="sbuild__author-block-right">
-							<p class="sbuild__author-name">
+						<div class="build__footer-desc">
+							<p class="build__author-name">
 								{{ loadedUserData.displayName }}
 							</p>
-							<p class="sbuild__author-date">
-								{{
-									new Date(
-										item.data.createdAt.seconds * 1000
-									).toLocaleDateString()
-								}}
-							</p>
-						</div>
-					</div>
-				</template>
-				<!-- else if we don't have any data (deleted user) -->
-				<template v-else>
-					<div class="sbuild__author-block">
-						<UserAvatar
-							:photo-u-r-l="'https://place-hold.it/80x80/8c8f94/8c8f94.jpg'"
-							:avatar-provider="'google'"
-							class="sbuild__author-avatar"
-						/>
-						<div class="sbuild__author-block-right">
-							<p class="sbuild__author-name">Пользователь не найден</p>
-							<p class="sbuild__author-date">—</p>
-						</div>
-					</div>
-				</template>
-			</div>
-
-			<!-- Icons column -->
-			<div class="sbuild__info-col">
-				<div class="sbuild__icons-top">
-					<div
-						class="sbuild__likes-block"
-						:class="{ active: isLikedByCurrentUser }"
-					>
-						<div class="sbuild__icon-container">
-							<div class="sbuild__icon-main">
-								<svg
-									width="20"
-									height="16"
-									viewBox="0 0 20 16"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-									class="sbuild__likes-svg"
+							<div class="build__footer-dates-wrapper">
+								<div class="build__footer-date-item">
+									<IconBase :width="14" :height="14">
+										<IconMagic />
+									</IconBase>
+									<span class="build__date">
+										{{
+											new Date(
+												item.data.createdAt.seconds * 1000
+											).toLocaleDateString()
+										}}
+									</span>
+								</div>
+								<div
+									class="build__footer-date-item"
+									v-if="
+										item.data.updatedAt &&
+										item.data.updatedAt.seconds !== item.data.createdAt.seconds
+									"
 								>
-									<path
-										d="M9.62064 2.29822C9.71563 2.40892 9.85422 2.47262 10.0001 2.47262C10.146 2.47262 10.2846 2.40892 10.3795 2.29822C12.042 0.360759 14.697 -0.164272 16.7433 1.53117C18.8279 3.25825 19.0829 6.18754 17.4711 8.25512C16.761 9.16591 15.3047 10.6111 13.8175 12.0104C12.3398 13.4007 10.8626 14.7173 10.1329 15.3615C10.0911 15.3983 10.0585 15.4271 10.0298 15.4516C10.0158 15.4637 10.004 15.4736 9.99388 15.4818C9.96318 15.4567 9.92162 15.4203 9.85502 15.3615L9.85497 15.3615C9.12523 14.7173 7.64801 13.4007 6.17037 12.0104C4.68313 10.6111 3.22682 9.16591 2.51676 8.25513C0.90935 6.19323 1.19206 3.2793 3.24966 1.5268L2.92546 1.14615L3.24966 1.5268C5.25246 -0.179029 7.95452 0.35653 9.62064 2.29822Z"
-										fill="#a5a5a5"
-										stroke="#a5a5a5"
-										stroke-linecap="round"
-										stroke-linejoin="round"
-									/>
-								</svg>
+									<IconBase :width="12" :height="12">
+										<IconPen />
+									</IconBase>
+									<span class="build__date">
+										{{
+											new Date(
+												item.data.updatedAt.seconds * 1000
+											).toLocaleDateString()
+										}}
+									</span>
+								</div>
 							</div>
 						</div>
-
-						<span class="sbuild__likes-amount">{{ likesAmountOnLoad }}</span>
 					</div>
-
-					<!-- Shows all buttons (visibility, cloned) -->
-					<template v-if="isStatusVisible">
-						<!-- Visibility -->
-						<div class="tooltip-anchor" v-if="item.data.isPublic">
-							<TooltipComponent :direction="'left'" :width="22">
-								<p>Видна всем пользователям</p>
-							</TooltipComponent>
-							<div class="sbuild__card">
-								<IconBase :iconColor="'#a5a5a5'">
-									<IconGlobe />
-								</IconBase>
-							</div>
-						</div>
-						<div class="tooltip-anchor" v-else>
-							<TooltipComponent :direction="'left'" :width="22">
-								<p>Видна только вам</p>
-							</TooltipComponent>
-							<div class="sbuild__card">
-								<IconBase :iconColor="'#a5a5a5'">
-									<IconEyeSlash />
-								</IconBase>
-							</div>
-						</div>
-						<!-- End of Visibility -->
-						<!-- Cloned -->
-						<div class="tooltip-anchor" v-if="item.data.isCloned">
-							<TooltipComponent :direction="'left'" :width="22">
-								<p>Сборка является копией</p>
-							</TooltipComponent>
-							<div class="sbuild__card">
-								<IconBase :iconColor="'#a5a5a5'">
-									<IconCopy />
-								</IconBase>
-							</div>
-						</div>
-					</template>
-				</div>
-
-				<div class="sbuild__icons-bottom">
-					<template v-if="isStatusVisible">
-						<div class="tooltip-anchor">
-							<TooltipComponent :direction="'left'" :width="10">
-								<p>Удалить</p>
-							</TooltipComponent>
-							<div class="sbuild__card">
-								<IconBase :iconColor="'#a5a5a5'">
-									<IconTrash />
-								</IconBase>
-							</div>
-						</div>
-					</template>
-				</div>
+				</template>
 			</div>
 		</section>
 	</template>
@@ -229,6 +164,8 @@ import IconGlobe from '@/components/ui/icon/icons/IconGlobe.vue';
 import IconEyeSlash from '@/components/ui/icon/icons/IconEyeSlash.vue';
 import IconTrash from '@/components/ui/icon/icons/IconTrash.vue';
 import IconCopy from '@/components/ui/icon/icons/IconCopy.vue';
+import IconMagic from '@/components/ui/icon/icons/IconMagic.vue';
+import IconPen from '@/components/ui/icon/icons/IconPen.vue';
 import IconMobility from '@/components/ui/icon/icons/IconMobility.vue';
 import IconVitality from '@/components/ui/icon/icons/IconVitality.vue';
 import IconWeapon from '@/components/ui/icon/icons/IconWeapon.vue';
@@ -273,6 +210,8 @@ export default defineComponent({
 		IconEyeSlash,
 		IconTrash,
 		IconCopy,
+		IconMagic,
+		IconPen,
 		IconMobility,
 		IconVitality,
 		IconWeapon,
