466e82b98829c6a7acc003f77616e321ce7ad9f5
Ullus <nikita96piter@gmail.com>
2025-10-04 03:44:14 +0300

    Feat(CompactBuildsBlock): add filter reactivity and builds count tracking

--- src/components/build-compact/CompactBuildsBlock.vue
+++ src/components/build-compact/CompactBuildsBlock.vue
@@ -3,9 +3,36 @@
 		<div class="ccard__header">
 			<h3 class="ccard__title">Сборки: Мои и избранные</h3>
 			<div class="ccard__chips">
-				<div class="chip ccard__chip active" v-ripple>Все</div>
-				<div class="chip ccard__chip" v-ripple>Мои (3)</div>
-				<div class="chip ccard__chip" v-ripple>Избранные (2)</div>
+				<div
+					class="chip ccard__chip"
+					:class="{ active: chosenFilter === 'all' }"
+					v-ripple
+					@click="setFilter('all')"
+				>
+					Все
+				</div>
+				<div
+					class="chip ccard__chip"
+					:class="{ active: chosenFilter === 'my' }"
+					v-ripple
+					@click="setFilter('my')"
+				>
+					Мои
+					<template v-if="myBuildsAmount > 0">
+						({{ myBuildsAmount }})
+					</template>
+				</div>
+				<div
+					class="chip ccard__chip"
+					:class="{ active: chosenFilter === 'liked' }"
+					v-ripple
+					@click="setFilter('liked')"
+				>
+					Избранные
+					<template v-if="likedBuildsAmount > 0">
+						({{ likedBuildsAmount }})
+					</template>
+				</div>
 			</div>
 		</div>
 
@@ -13,11 +40,11 @@
 			<CompactBuildCard v-for="item in 5" />
 		</div>
 
-		<p class="ccard__tutorial">
-			<template v-if="true">
+		<p class="ccard__tutorial" v-if="hasLikedAndOwnedBuilds">
+			<template v-if="myBuildsAmount === 0">
 				♡ Создайте свою, или лайкните сборки других и они появятся здесь!
 			</template>
-			<template v-if="false">
+			<template v-else-if="likedBuildsAmount === 0">
 				♡ Лайкните сборки других и они появятся здесь!
 			</template>
 		</p>
@@ -26,7 +53,10 @@
 			<span class="ccard__load-more-desc">
 				Есть еще
 				<span class="ccard__load-more-number"
-					>{{ Math.floor(Math.random() * 15 + 1) }} сборок</span
+					>{{ buildsLeftUnloaded }}
+					{{
+						getDeclension(buildsLeftUnloaded, ['сборка', 'сборки', 'сборок'])
+					}}</span
 				>
 			</span>
 			<button class="btn btn-m btn-secondary">Показать еще</button>
@@ -35,13 +65,44 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, ref, Ref } from 'vue';
 import CompactBuildCard from './CompactBuildCard.vue';
 
 export default defineComponent({
 	components: { CompactBuildCard },
 	setup() {
-		return {};
+		type Filter = 'all' | 'my' | 'liked';
+		const chosenFilter: Ref<Filter> = ref('all');
+		function setFilter(filter: Filter) {
+			chosenFilter.value = filter;
+		}
+
+		const myBuildsAmount: Ref<number> = ref(2);
+		const likedBuildsAmount: Ref<number> = ref(1);
+		const hasLikedAndOwnedBuilds = computed(
+			() => !!(myBuildsAmount.value + likedBuildsAmount.value)
+		);
+
+		const buildsLeftUnloaded: Ref<number> = ref(5);
+
+		function getDeclension(number: number, titles: [string, string, string]) {
+			const cases = [2, 0, 1, 1, 1, 2];
+			return titles[
+				number % 100 > 4 && number % 100 < 20
+					? 2
+					: cases[number % 10 < 5 ? number % 10 : 5]
+			];
+		}
+
+		return {
+			chosenFilter,
+			setFilter,
+			myBuildsAmount,
+			likedBuildsAmount,
+			hasLikedAndOwnedBuilds,
+			buildsLeftUnloaded,
+			getDeclension,
+		};
 	},
 });
 </script>
